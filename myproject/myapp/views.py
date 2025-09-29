e2ea9888d038150fbd7589cc47102f38ca43a7fd
Salim <narsbekov_s@iuca.kg>
2025-09-29 15:41:22 +0600

    button delete

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
 
-def home(request):
-    return HttpResponse("Привет! Это моё первое приложение Django 🚀")
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -11,3 +11,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),  # подключаем наше приложение
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+]
+
